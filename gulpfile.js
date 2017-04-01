e6a616b362201576db9385887da212f9534d66d3
benweizhu <benweizhu@gmail.com>
2017-04-01 17:09:33 +0800

    add sass task and static page

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var iconfont = require('gulp-iconfont');
 var iconfontCss = require('gulp-iconfont-css');
+var sass = require('gulp-sass');
 
 var fontName = 'Icons';
 
@@ -8,12 +9,18 @@ gulp.task('iconfont', function(){
   gulp.src(['app/assets/icons/*.svg'])
   .pipe(iconfontCss({
     fontName: fontName,
-    path: 'app/assets/css/templates/_icons.scss',
-    targetPath: '../../css/_icons.scss',
-    fontPath: '../../fonts/icons/'
+    path: 'app/assets/scss/templates/_icons.scss',
+    targetPath: '../../scss/_icons.scss',
+    fontPath: '../fonts/icons/'
   }))
   .pipe(iconfont({
     fontName: fontName
   }))
   .pipe(gulp.dest('app/assets/fonts/icons/'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./app/assets/scss/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./app/assets/css'));
 });
