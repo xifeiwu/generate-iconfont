3d4178e057db84fdbf831c17e85140482b46eb44
benweizhu <benweizhu@gmail.com>
2017-04-03 11:53:01 +0800

    watch change and automated regenerate

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,48 @@
+var fs = require('fs');
+
 var gulp = require('gulp');
 var iconfont = require('gulp-iconfont');
 var iconfontCss = require('gulp-iconfont-css');
 var sass = require('gulp-sass');
 var server = require('gulp-express');
+var template = require('gulp-template');
 
 var fontName = 'Icons';
 
-gulp.task('iconfont', function(){
-  gulp.src(['app/assets/icons/*.svg'])
-  .pipe(iconfontCss({
-    fontName: fontName,
-    path: 'app/assets/scss/templates/_icons.scss',
-    targetPath: '../../scss/_icons.scss',
-    fontPath: '../fonts/icons/'
-  }))
-  .pipe(iconfont({
-    fontName: fontName
-  }))
-  .pipe(gulp.dest('app/assets/fonts/icons/'));
+gulp.task('iconfont', function () {
+	gulp.src(['app/assets/icons/*.svg'])
+	.pipe(iconfontCss({
+		fontName: fontName,
+		path: 'app/assets/scss/templates/_icons.scss',
+		targetPath: '../../scss/_icons.scss',
+		fontPath: '../fonts/icons/'
+	}))
+	.pipe(iconfont({
+		fontName: fontName
+	}))
+	.pipe(gulp.dest('app/assets/fonts/icons/'));
 });
 
-gulp.task('sass',['iconfont'], function () {
-  return gulp.src('./app/assets/scss/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./app/assets/css'));
+gulp.task('sass', ['iconfont'], function () {
+	return gulp.src('./app/assets/scss/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('./app/assets/css'));
 });
 
-gulp.task('server', ['sass'], function () {
-  server.run(['app.js']);
-  // Restart the server when file changes
-  gulp.watch(['app/**/*.html'], server.notify);
-  gulp.watch(['app/assets/**/*.scss'], ['sass']);
-  gulp.watch(['app/assets/**/*.svg'], ['sass']);
+gulp.task('server', ['sass', 'template'], function () {
+	server.run(['app.js']);
+	gulp.watch(['app/**/*.html'], server.notify);
+	gulp.watch(['app/assets/**/*.scss'], ['sass', 'template']);
+	gulp.watch(['app/assets/**/*.html'], ['sass', 'template']);
+	gulp.watch(['app/assets/**/*.svg'], ['sass', 'template']);
+});
+
+gulp.task('template', function () {
+	gulp.src('app/assets/template/index.html')
+	.pipe(template({
+		'icons': fs.readdirSync('./app/assets/icons/').map(function (name) {
+			return name.replace(/\.[^/.]+$/, '');
+		})
+	}))
+	.pipe(gulp.dest('app'));
 });
