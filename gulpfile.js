b8b69df4177b61621cd9ae6c62947d35a26aef9f
wuxifei <wuxifei@finupgroup.com>
2018-02-05 16:41:00 +0800

    organize structure of project

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-
+var del = require('del');
 var gulp = require('gulp');
 var iconfont = require('gulp-iconfont');
 var iconfontCss = require('gulp-iconfont-css');
@@ -10,43 +10,54 @@ var template = require('gulp-template');
 var fontName = 'my-icons';
 
 gulp.task('iconfont', function () {
-	gulp.src(['app/assets/icons/*.svg'])
-	.pipe(iconfontCss({
-		fontName: fontName,
-		path: 'app/assets/scss/templates/_icons.scss',
-		cssClass: 'my-icon',
-		targetPath: '../../scss/_icons.scss',
-		fontPath: '../fonts/icons/',
-		centerHorizontally: true
-	}))
-	.pipe(iconfont({
-		fontName: fontName,
-		fontHeight: 1001, //(>= 1000)
-		normalize: true
-	}))
-	.pipe(gulp.dest('app/assets/fonts/icons/'));
+  gulp.src(['assets/icons/*.svg'])
+  .pipe(iconfontCss({
+    fontName: fontName,
+    path: 'assets/scss/_icons.scss',
+    cssClass: 'my-icon',
+    targetPath: '../scss/_icons.scss',
+    fontPath: '../fonts/',
+    centerHorizontally: true
+  }))
+  .pipe(iconfont({
+    fontName: fontName,
+    fontHeight: 1001, //(>= 1000)
+    normalize: true
+  }))
+  .pipe(gulp.dest('output/assets/fonts/'));
 });
 
-gulp.task('sass', ['iconfont'], function () {
-	return gulp.src('./app/assets/scss/*.scss')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('./app/assets/css'));
+gulp.task('font-scss', ['iconfont'], function() {
+  return gulp.src('assets/scss/my-icon.scss')
+      .pipe(gulp.dest('output/assets/scss/'));
 });
 
-gulp.task('server', ['sass', 'template'], function () {
-	server.run(['app.js']);
-	gulp.watch(['app/**/*.html'], server.notify);
-	gulp.watch(['app/assets/**/*.scss'], ['sass', 'template']);
-	gulp.watch(['app/assets/**/*.html'], ['sass', 'template']);
-	gulp.watch(['app/assets/**/*.svg'], ['sass', 'template']);
+gulp.task('sass', ['font-scss'], function () {
+  return gulp.src('./output/assets/scss/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./output/assets/css'));
 });
 
 gulp.task('template', function () {
-	gulp.src('app/assets/template/index.html')
-	.pipe(template({
-		'icons': fs.readdirSync('./app/assets/icons/').map(function (name) {
-			return name.replace(/\.[^/.]+$/, '');
-		})
-	}))
-	.pipe(gulp.dest('app'));
+  gulp.src('assets/template/index.html')
+  .pipe(template({
+    'icons': fs.readdirSync('./assets/icons/').map(function (name) {
+      return name.replace(/\.[^/.]+$/, '');
+    })
+  }))
+  .pipe(gulp.dest('output'));
+});
+
+gulp.task('clean', function() {
+    return del.sync(['./output/*'], {
+        force: true
+    });
+});
+
+gulp.task('server', ['clean','sass', 'template'], function () {
+  server.run(['app.js']);
+  gulp.watch(['**/*.html'], server.notify);
+  gulp.watch(['assets/**/*.scss'], ['sass', 'template']);
+  gulp.watch(['assets/**/*.html'], ['sass', 'template']);
+  gulp.watch(['assets/**/*.svg'], ['sass', 'template']);
 });
